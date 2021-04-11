8ee16f95b9d125e9fa8c7c498efadf8615983965
Emanuel Malig <emalig@ucsc.edu>
2021-04-11 15:46:57 -0700

    canvas setup

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-let game = new Phaser.Game();
+const { Phaser } = require("../lib/phaser");
+
+let config ={
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480
+}
+let game = new Phaser.Game(config);
