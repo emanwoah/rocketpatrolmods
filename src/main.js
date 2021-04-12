f0e1e628d25d8cc8095ec7050b1493cd319928e6
Emanuel Malig <emalig@ucsc.edu>
2021-04-11 23:40:44 -0700

    master copy

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ let config = {
     type: Phaser.AUTO,
     width: 640,
     height: 480,
-    scene: [Menu, Play]
+    scene: [Menu, Play],
 }
 
 let game = new Phaser.Game(config);
@@ -10,3 +10,6 @@ let game = new Phaser.Game(config);
 //Set UI Sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+
+// reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -1,18 +1,29 @@
-// Spaceship prefab
+//spaceship prefab
 class Spaceship extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame, pointValue) {
-        super(scene, x, y, texture, frame, pointValue);
-        scene.add.existing(this); // add to existing scene
-        this.points = pointValue; // store to pointValue
-        this.moveSpeed = 3; //pixels per frame
+        super (scene, x, y, texture, frame);
+        //add to scene
+        scene.add.existing(this);
+
+        //store point value
+        this.points = pointValue;
+
+        //move speed
+        this.moveSpeed = game.settings.spaceshipSpeed;
     }
 
     update() {
         // move spaceship to the left
         this.x -= this.moveSpeed;
-        // wrap around from left edge to the right edge
-        if(this.x <= 0 - this.width) {
+
+        //wrap around from left edge to right edge
+        if(this.x <=0 - this.width) {
             this.x = game.config.width;
         }
+    }
+
+    //reset position
+    reset() {
+        this.x = game.config.width;
     }
 }
